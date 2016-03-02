1b35603f15b8fadcb7a409346ccc5260d0be61e0
CaptainAtlas <daniel.bye@colorado.edu>
2016-03-02 09:02:01 -0700

    Added initial GPS code

---
+++ gps/gps_test.py
@@ -0,0 +1,22 @@
+import gps
+
+# Listen to gpsd on port 2947
+session = gps.gps("localhost", "2947")
+session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+
+while True:
+	try:
+		report = session.next()
+			#Narrow fields with classes
+			#To see everything, uncomment the line below
+			#print report
+		if report['class'] = 'TPV':
+			if hasattr(report, 'time'):
+				print report.time
+	except KeyError:
+		pass
+	except KeyboardInterrupt:
+		quit()
+	except StopIteration:
+		session = None
+		print "GPSD Timeout"
